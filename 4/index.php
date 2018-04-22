e539b61a23ec0e68bfe24ef21fbbd4e6aaa108b0
Alexander Iliev <azxsasho@gmail.com>
2018-04-22 12:25:07 +0300

    Homework 4 - Add: solution

---
+++ 4/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<HTML>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Избираеми дисциплини</title>
+</head>
+
+<body>
+
+<div id="account">
+    <button class="account">Вход</button>
+    <button class="account">Регистрация</button>
+</div>
+
+<div id="sidebar">
+    <a href="#" class="selected with-bottom-border">КОМПЮТЪРНА ГРАФИКА С WEBGL</a>
+    <a href="#" class="with-bottom-border">ПРОГРАМИРАНЕ С GO</a>
+    <a href="#" class="with-bottom-border">ПРОГРАМИРАНЕ С RUBY</a>
+    <a href="#" class="with-bottom-border">AGILE</a>
+</div>
+
+<div id="content">
+    <h1 class="with-bottom-border">Компютърна графика с WebGL</h1>
+    <h2>доц. П.Бойчев</h2>
+    <p>След завършване на курса студентите ще могат да създават примерни статични изображения и тримерни анимации</p>
+
+    <p><i>Тип: </i>задължителен избираем</p>
+    <p><i>Курс: </i>1<sup>ви</sup></p>
+    <p><i>Програма: </i>бакалавър</p>
+
+    <textarea placeholder="Добавяне на коментар" name="Text1" cols="40" rows="5"></textarea>
+
+    <div>
+        <button class="negative">Отказ</button>
+        <button>Публикуване</button>
+    </div>
+
+    <h2>Коментари</h2>
+
+    <div class="comment with-bottom-border">
+        <div class="comment-info">
+            <a href="#"> Вили Велева</a>
+            <p>07.11.2015</p>
+        </div>
+        <p>"Препоръчвам записването на курса. Интересни неща се учат, има и освобождаване с реферати."</p>
+    </div>
+
+    <div class="comment with-bottom-border">
+        <div class="comment-info">
+            <a href="#"> Вили Велева</a>
+            <p>07.11.2015</p>
+        </div>
+        <p>"Препоръчвам записването на курса. Интересни неща се учат, има и освобождаване с реферати."</p>
+    </div>
+
+    <div class="comment with-bottom-border">
+        <div class="comment-info">
+            <a href="#"> Вили Велева</a>
+            <p>07.11.2015</p>
+        </div>
+        <p>"Препоръчвам записването на курса. Интересни неща се учат, има и освобождаване с реферати."</p>
+    </div>
+</div>
+</body>
+</HTML>
